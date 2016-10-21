bb638d64e213446ba552ac1a0a786c7e068694e5
Michael Schönitzer <schoenitzer@wikipedia.de>
2016-10-21 16:36:02 +0200

    Updated Skel

--- createskel.sh
+++ createskel.sh
@@ -2,10 +2,10 @@
 
 scp -r root@riem:/home/gast/* roles/useraccounts/files/etc/skel
 scp -r root@riem:/home/gast/.* roles/useraccounts/files/etc/skel
-git rm -r roles/useraccounts/files/etc/skel/.cache/
-git rm -r roles/useraccounts/files/etc/skel/.Xauthority
-git rm -r roles/useraccounts/files/etc/skel/.xsession-errors
-git rm -r roles/useraccounts/files/etc/skel/.dbus/session-bus/*
+git rm -rf roles/useraccounts/files/etc/skel/.cache/
+git rm -rf roles/useraccounts/files/etc/skel/.Xauthority
+git rm -rf roles/useraccounts/files/etc/skel/.xsession-errors
+git rm -rf roles/useraccounts/files/etc/skel/.dbus/session-bus/*
 
 git add -A
-git commit -a -m "Updated Skel"
+#git commit -a -m "Updated Skel"
