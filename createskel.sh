cde4d9750039c7c3568b4fc2e1fc8bf13c754806
Michael Schönitzer <schoenitzer@wikipedia.de>
2016-10-21 22:34:09 +0200

    Improved create-skel-script

--- createskel.sh
+++ createskel.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-scp -r root@riem:/home/gast/* roles/useraccounts/files/etc/skel
-scp -r root@riem:/home/gast/.* roles/useraccounts/files/etc/skel
+if [ $# -eq 0 ] ; then
+  echo "No hostname given, using riem"
+  hostname=riem
+elif [ $# -eq 1 ] ; then
+  echo "Using $1 as source-host"
+  hostname=$1
+else
+  echo "Wrong usage"
+  echo ""
+  echo "$0 [hostname]"
+  exit
+fi
+
+scp -r root@${hostname}:/home/gast/* roles/useraccounts/files/etc/skel
+scp -r root@${hostname}:/home/gast/.* roles/useraccounts/files/etc/skel
 git rm -rf roles/useraccounts/files/etc/skel/.cache/
 git rm -rf roles/useraccounts/files/etc/skel/.Xauthority
 git rm -rf roles/useraccounts/files/etc/skel/.xsession-errors
 git rm -rf roles/useraccounts/files/etc/skel/.dbus/session-bus/*
 
 git add -A
-#git commit -a -m "Updated Skel"
+git commit -a -m "Updated Skel"
+ansible-playbook main.yml -t skel
